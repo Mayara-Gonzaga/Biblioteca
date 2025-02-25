4015969dc9e5286dd7a32935f1744dd04917a8de
Mayara Gonzaga <mayaragonzagag@gmail.com>
2025-02-24 23:48:54 -0300

    Ajustes para deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import BookView from './Components/BookView';
+import BookForm from './Components/BookForm';
+import EditBook from './Components/EditBook';
+import BookList from './Components/BookList';
+import Home from './Components/Home';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    {/* Rota para a página inicial */}
+                    <Route path="/" element={<Home />} />
+
+                    {/* Rota para listar todos os livros */}
+                    <Route path="/books" element={<BookList />} />
+
+                    {/* Rota para visualizar um livro específico */}
+                    <Route path="/books/view/:id" element={<BookView />} />
+
+                    {/* Rota para adicionar um novo livro */}
+                    <Route path="/books/new" element={<BookForm />} />
+
+                    {/* Rota para editar um livro existente */}
+                    <Route path="/books/edit/:id" element={<EditBook />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
